8e7558e5c78bf9be66ce3bc75304c44dddb07dfb
Benjamin Roth <beroth@cis.uni-muenchen.de>
2018-10-25 12:04:43 +0200

    Added Homework hw02_objects.

---
+++ src/hw02_objects/oop_1_bank.py
@@ -0,0 +1,37 @@
+"""
+Exercise 1: (5 points)
+
+a) Using the slides & the script, put together a file
+    containing the complete Account class.
+    Each method must have a documentation string at the
+    beginning which describes what the method is doing.
+    (1 points)
+
+b) Create a main application where you create a number of accounts.
+    Play around with depositing / withdrawing money.
+    Change the account holder of an account using a setter method.
+    (1 point)
+
+c) Change the withdraw function such that the minimum balance
+    allowed is -1000.
+    (1 point)
+
+d) Write a function apply_interest(self) which applies an interest
+    rate of 1.5% to the current balance and call it on your objects.
+    (1 points)
+
+e) Draw a UML diagram representing your Account class. (1 point)
+
+"""
+
+
+class Account:
+    """ Here has to be a documentation string that describes
+    which data objects this class is designed for.
+    You have to remove the pass statement and then write some
+    code for the class. """
+    pass
+
+if __name__ == "__main__":
+    print("Welcome to the Python Bank!")
+
---
+++ src/hw02_objects/oop_2_employee.py
@@ -0,0 +1,18 @@
+"""
+Exercise 2: (4 points)
+
+a) Write the complete code for the Employee class
+    (including constructor, __str__,...) (2 points)
+
+b) Create a few employee objects and show how you can
+    manipulate them using the methods. (1 points)
+
+c) Draw a UML class diagram for your Employee class. (1 point)
+"""
+
+
+class Employee:
+    pass
+
+if __name__ == "__main__":
+    print("Employee application")
