2eea5e70de31883040cb2783d4df570324de2a67
Haotian Ye <yehaotian@HT-MBP-DE.local>
2018-11-07 18:43:16 +0100

    finished

--- src/hw03_documents/document.py
+++ src/hw03_documents/document.py
@@ -1,29 +1,47 @@
+import nltk
+
 def normalized_tokens(text):
     """ This takes a string and returns lower-case tokens, using nltk for tokenization. """
-    pass # TODO: return list with lower-case tokens.
+    return nltk.word_tokenize(text.lower())
+
+def word_counts(text):
+    wdic = {}
+    for word in normalized_tokens(text):
+        if word in wdic:
+            wdic[word] +=1
+        else:
+            wdic[word] = 1
+    return wdic
 
 class TextDocument:
     def __init__(self, text, id=None):
         """ This creates a from a string and an identifier. """
         self.text = text
-        self.word_to_count = None # TODO: Create dictionary from words to counts.
+        self.word_to_count = word_counts(text)
         self.id = id
 
     @classmethod
     def from_file(cls, filename):
         """ This creates a TextDokument by reading a file. """
-        text = "" # TODO: read text from filename
+        file = open(filename, "r")
+        text = file.read()
+        file.close()
         return cls(text, filename)
 
     def __str__(self):
         """ This returns a short string representation, which is at most 25 characters long.
         If the original text is longer than 25 characters, the last 3 characters of the short string should be '...'.
         """
-        pass # TODO: Implement correct return statement.
+        if len(self.text) > 25:
+            return self.text[0:22] + "..."
+        else:
+            return self.text
 
     def word_overlap(self, other_doc):
         """ This returns the number of words that occur in both of the documents (self and other_doc) at the same time.
         Every word should be considered only once, irrespective of how often it occurs in either document (i.e. we
         consider word *types*).
         """
-        pass # TODO: Implement correct return statement.
+        list1 = {word for word in normalized_tokens(self.text)}
+        list2 = {word for word in normalized_tokens(other_doc.text)}
+        return len(list1.intersection(list2))
