c119d5a96184496e4b0227288d9824f3033c37f0
Haotian Ye <yehaotian@HT-MBP-DE.local>
2018-11-14 18:24:02 +0100

    comprehension added

--- src/hw04_text_search/comprehensions.py
+++ src/hw04_text_search/comprehensions.py
@@ -6,7 +6,7 @@ def multiply_by(x, list1):
     """
     Multiplies each value in list1 by x and returns it as a new list.
     """
-    return None  # TODO: replace
+    return [i * x for i in list1]
 
 
 def check_division(x, list1):
@@ -14,7 +14,7 @@ def check_division(x, list1):
     Takes a list and returns a list indicating whether or not each element in the original list can be divided by x.
     (e.g check\_division(3, [1,2,3]) -> [False, False, True])
     """
-    return None  # TODO: replace
+    return [True if i%x == 0 else False for i in list1]
 
 
 def div_less(set1):
@@ -22,8 +22,7 @@ def div_less(set1):
     Return a new set only containing numbers that can`t be divided by any other number (except itself)
     from the original set.
     """
-    return None  # TODO: replace
-
+    return {i for i in set1 if len([j for j in set1 if i%j == 0 and i != j]) == 0}
 
 def map_zip(list1, list2):
     """
@@ -31,11 +30,11 @@ def map_zip(list1, list2):
     Make use of the 'zip()' function in your dictionary comprehension, that can handle lists of different sizes
     automatically.
     """
-    return None  # TODO: replace
+    return {tup[0]:tup[1] for tup in zip(list1, list2)}
 
 
 def word_to_length(list1):
     """
     Returns a dictionary mapping all words with at least 3 characters to their number of characters.
     """
-    return None  # TODO: replace
+    return {word:len(word) for word in list1 if len(word) >= 3}
---
+++ src/hw04_text_search/test_comprehensions_new.py
@@ -0,0 +1,41 @@
+import unittest
+
+from hw04_text_search.comprehensions import multiply_by, check_division, div_less, map_zip, word_to_length
+
+
+class TestComprehensions(unittest.TestCase):
+
+    def setUp(self):
+        self.numberList = [-2, -3, 4, 5, 7, 43, 2345, 1343, 13, 40, 24, 221, 5942, 7, 9, 113]
+        self.wordList = "All my words are unique and therefore not duplicated".split()
+
+    def test_multiply_by(self):
+        expected = [4, 6, -8, -10, -14, -86, -4690, -2686, -26, -80, -48, -442, -11884, -14, -18, -226]
+        got = multiply_by(-2, self.numberList)
+        self.assertEqual(got, expected)
+
+    def test_check_division(self):
+        expected = [False, True, False, False, False, False, False, False, False, False, True, False, False, False,
+                    True, False]
+        got = check_division(3, self.numberList)
+        self.assertEqual(got, expected)
+
+    def test_div_less(self):
+        expected = {-2, -3, 5, 7, 43, 1343, 13, 113}
+        got = div_less(set(self.numberList))
+        self.assertEqual(got, expected)
+
+    def test_map_zip(self):
+        expected = {'All': -2, 'my': -3, 'words': 4, 'are': 5, 'unique': 7, 'and': 43, 'therefore': 2345, 'not': 1343,
+                    'duplicated': 13}
+        got = map_zip(self.wordList, self.numberList)
+        expected2 = {-2: 'All', -3: 'my', 4: 'words', 5: 'are', 7: 'unique', 43: 'and', 2345: 'therefore', 1343: 'not',
+                     13: 'duplicated'}
+        got2 = map_zip(self.numberList, self.wordList)
+        self.assertEqual(got, expected)
+        self.assertEqual(got2, expected2)
+
+    def test_word_to_length(self):
+        expected = {'All': 3, 'words': 5, 'are': 3, 'unique': 6, 'and': 3, 'therefore': 9, 'not': 3, 'duplicated': 10}
+        got = word_to_length(self.wordList)
+        self.assertDictEqual(got, expected)
