2855af7e209a655a5b2dc50e95e86e5f70e35b05
Wabro <Leo.Wabro@campus.lmu.de>
2018-12-19 18:28:54 +0100

    model_lang fertig

--- src/hw08_nltk/model_lang.py
+++ src/hw08_nltk/model_lang.py
@@ -9,7 +9,7 @@ class LangModeler(object):
     def build_language_models(self):
         # TODO return ConditionalFrequencyDistribution of words in the UDHR corpus conditioned on each language
         # hint: use nltk.ConditionalFreqDist
-        pass
+        return nltk.ConditionalFreqDist([(lang, word.lower()) for lang in self.languages for word in self.words[lang]])
 
     def guess_language(self,language_model_cfd, text):
         """Returns the guessed language for the given text"""
@@ -17,4 +17,11 @@ class LangModeler(object):
         #TODO for each language calculate the overall score of a given text
         #based on the frequency of words accessible by language_model_cfd[language].freq(word) and then
         #identify most likely language for a given text according to this score
-        pass
+
+        prob_lang = []
+
+        for language in self.languages:
+            prob = sum(language_model_cfd[language].freq(token) for token in nltk.word_tokenize(text) if token not in ".?,")
+            prob_lang.append((language, prob))
+
+        return sorted(prob_lang, key=lambda x: x[1], reverse=True)[0][0]
